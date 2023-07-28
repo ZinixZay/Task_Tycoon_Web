6fb23866bd317d8e995e5495b533cec918bf31c3
ZinixZay <romadesl21@gmail.com>
2023-07-28 18:54:34 +0300

    task creation html added

--- task_tycoon/tasks/views.py
+++ task_tycoon/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic import View
 
 from .forms import AddTaskForm, TaskStructureForm
@@ -7,21 +7,12 @@ from .models import Task
 # Create your views here.
 
 
-class MakeTaskStructure(View):
+class CreateTask(View):
     def post(self, request):
         form = TaskStructureForm(request.POST)
         if form.is_valid():
-            try:
-                Task.objects.create(**form.cleaned_data)
-                return redirect('createtask', context=form.cleaned_data)
-            except Exception:
-                form.add_error(None, 'Ошибка')
+            return redirect('createtask', context=form.cleaned_data)
+
 
     def get(self, request):
         return render(request, 'tasks/task-structure.html', {'form': TaskStructureForm, 'title': 'Структура задания'})
-
-
-class CreateTask(View):
-    def get(self, request, context):
-        print(**context)
-
