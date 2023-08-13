1d1e1b623e535f26f415ff5fffb035c2a86cd392
ZinixZay <romadesl21@gmail.com>
2023-08-13 13:55:59 +0300

    detail task styled, delete path created

--- task_tycoon/tasks/utils.py
+++ task_tycoon/tasks/utils.py
@@ -5,3 +5,13 @@ from random import randint
 
 def generate_identifier():
     return randint(100000, 999999)
+
+
+def parse_task_questions(questions_query_set: list) -> list:
+    questions = list()
+    for question in questions_query_set:
+        if question.variants:
+            for response in question.variants:
+                pass
+    return []
+
--- task_tycoon/tasks/views.py
+++ task_tycoon/tasks/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import View, ListView, DetailView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Task
+from .models import Task, Question
+from .utils import parse_task_questions
 
 
 '''
 Сделать пермишны для неавторизованных
 Добавить .env
+Валидаторы на задание
+Миксины
 '''
 
 menu = [
@@ -22,12 +27,12 @@ def index(request):
     return render(request, template_name='tasks/index.html', context={'menu': menu, 'title': 'Главная'})
 
 
-class CreateTask(View):
+class CreateTask(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'tasks/create_task.html', context={'title': 'Создание задания', 'menu': menu})
 
 
-class MyTasks(ListView):
+class MyTasks(LoginRequiredMixin, ListView):
     model = Task
     template_name = "tasks/tasks_view.html"
     context_object_name = 'tasks'
@@ -42,12 +47,29 @@ class MyTasks(ListView):
         return new_queryset
 
 
-class ShowTask(DetailView):
+class ShowTask(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'tasks/show_task.html'
     context_object_name = 'task'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = {'title': kwargs['object'].title, 'menu': menu}
+
+        questions = Question.objects.filter(task_id=self.kwargs['pk'])
+        # questions = parse_task_questions(questions_query)
+
+        c_def = {'title': kwargs['object'].title, 'menu': menu, 'questions': questions}
         return {**context, **c_def}
+
+
+class DeleteTask(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/delete_task.html'
+    success_url = reverse_lazy('tasks')
+    context_object_name = 'task'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = {'title': 'Удаление задания', 'menu': menu}
+        return {**context, **c_def}
+
